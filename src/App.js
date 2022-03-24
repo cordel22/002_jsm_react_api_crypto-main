76a1b6d77d76c947e890d8a7b86ae1d14f158f7e
cordel22 <cordelfenevall@gmail.com>
2022-03-24 19:49:18 +0700

    3:18:17 modify dont change app and save will render

---
+++ src/App.js
@@ -0,0 +1,105 @@
+// import React from 'react';
+// import { Switch, Route, Link } from 'react-router-dom';
+//  import { Layout, Typography, Space } from 'antd';
+
+import { Navbar, Exchanges, Homepage, Cryptocurrencies, News, CryptoDetails/* , Layout, Footer */ } from './components';
+import './App.css';
+
+import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LayKokotout from "./pages/LayKokotout";
+import Home from "./pages/Home";
+import Blogs from "./pages/Blogs";
+import Contacts from "./pages/Contacts";
+import NoPage from "./pages/NoPage";
+
+
+
+
+export default function App() {
+  return (
+
+        <div className="app">
+          <div className="navbar">
+            
+          </div>
+          <div className="main">
+
+          <div className="routes">
+        
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<LayKokotout />}>
+                <Route index element={<Homepage />} />
+                <Route path="/exchanges" element={<Exchanges />} />
+                <Route path="/cryptocurrencies" element={<Cryptocurrencies />} />
+                <Route path="/crypto/:coinId" element={<CryptoDetails />} />
+                <Route path="/news" element={<News />} />
+
+                <Route index element={<Home />} />
+                <Route path="blogs" element={<Blogs />} />
+                <Route path="contact" element={<Contacts />} />
+                <Route path="*" element={<NoPage />} />
+              </Route>
+            </Routes>
+          </BrowserRouter>
+
+          </div>
+        
+</div>
+      <div className="footer">
+
+      </div>
+    </div>
+
+  );
+}
+
+
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+
+
+
+
+
+/* 
+//  funguje
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LayKokotout />}>
+          <Route index element={<Home />} />
+          <Route path="blogs" element={<Blogs />} />
+          <Route path="contact" element={<Contacts />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+ */
+/* const App = () => {
+  return (
+    <div className="app">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main">
+
+      </div>
+      <div className="footer">
+
+      </div>
+    </div>
+  )
+}
+
+export default App */
